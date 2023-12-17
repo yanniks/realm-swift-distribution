66ff9b19f13c7d867b2e9dcd29f7af4b76c6c472
Yannik Ehlert <yannik.ehlert@claas.com>
2023-12-17 22:34:45 +0100

    Add binaries for version 10.45.0

--- Package.swift
+++ Package.swift
@@ -24,13 +24,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Realm",
-            url: "https://github.com/yanniks/realm-swift-distribution/releases/download/v10.44.0/Realm.spm.zip",
-            checksum: "ac357a712f6ea5cdade9a12014f2c79478774ff6508ef636f0d37fbb289d891e"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.45.0/Realm.spm.zip",
+            checksum: "5215a659194877250a3ace4037ff228e217a77cd56af566bcc5f55730654a896"
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/yanniks/realm-swift-distribution/releases/download/v10.44.0/RealmSwift@15.1.spm.zip",
-            checksum: "c5b9d4e60a0b218ac6b6e04231335405f5c8de60eca5bac2191b76650267aefa"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.45.0/RealmSwift@15.1.spm.zip",
+            checksum: "b8c4f8d88c574420a0668cb46d6b0d3cf30c4d0076128540ae2e092c538c04e4"
         ),
     ]
 )
