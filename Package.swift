a711fbe0d7a4289a30b364962390ddba0878ebe0
Yannik Ehlert <yannik.ehlert@claas.com>
2024-05-27 11:54:50 +0200

    Add binaries for version 10.50.1

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@
 
 import PackageDescription
 
+let version = "v10.50.1"
+
 let package = Package(
     name: "Realm",
     platforms: [
@@ -24,13 +26,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Realm",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.48.0/Realm.spm.zip",
-            checksum: "bb63b20a9d0b6651777c16549006a11af6b7556db6e2991ed57eadda62974979"
+            url: "https://github.com/realm/realm-swift/releases/download/\(version)/Realm.spm.zip",
+            checksum: "942fc39917f4d572d5a2aae6a115c9f50a0954e61351aed3553c84d63ad3f2dc"
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.48.0/RealmSwift@15.3.spm.zip",
-            checksum: "36d860d326237b9950d2650f1bc9000b1af0540ce1a9967ae5d7f28be7956456"
+            url: "https://github.com/realm/realm-swift/releases/download/\(version)/RealmSwift@15.4.spm.zip",
+            checksum: "208755d16d189372065e43901e5406e050197f98207142bc788487068f1a0843"
         ),
     ]
 )
